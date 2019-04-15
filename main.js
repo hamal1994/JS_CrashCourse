0c684e8785980c6637b19c143178d6d0b3502921
Hamal <hamalp@hotmail.co.uk>
2019-04-15 15:02:08 +0100

    Learnt new method instead of concatenation

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert("hello world");
+// string, numbers, boolean, null, undefined
+
+const name = 'Hamal';
+const age = 25;
+
+// Concatenation
+console.log('My name is ' +name + ' and I am ' + age );
+
+// Template String - ES6 new Version
+console.log(`My name is ${name} and I am ${age}`);
+
